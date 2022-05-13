738ff714f114714d00a6a0bb47dab09d186be19a
NaufalFiraas <naufalfiraas09@gmail.com>
2022-05-13 22:41:04 +0700

    menambahkan initial sholat dan quran page

---
+++ lib/views/quranpage/quran_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class QuranPage extends StatelessWidget {
+  const QuranPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Quran'),
+      ),
+    );
+  }
+}
---
+++ lib/views/sholatpage/sholat_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SholatPage extends StatelessWidget {
+  const SholatPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Sholat Page'),
+      ),
+    );
+  }
+}

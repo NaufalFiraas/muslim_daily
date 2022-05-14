d8e67a55526da3e398832c9ff0f03e3551dbc7e8
NaufalFiraas <naufalfiraas09@gmail.com>
2022-05-14 18:10:19 +0700

    menambahkan class SholatpageRepositories dan method deteminePosition

---
+++ lib/data/repositories/sholatpage_repositories.dart
@@ -0,0 +1,27 @@
+import 'package:geolocator/geolocator.dart';
+
+class SholatpageRepositories {
+  Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      throw Exception('Location services are enabled');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw Exception('Location permissions are denied');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      throw Exception(
+          'Location permissions are denied forever. Please check your device settings!');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+}

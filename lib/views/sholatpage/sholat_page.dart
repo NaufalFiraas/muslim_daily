3a868de12477ad38b99d084d6a5b363fb1d6f1b1
NaufalFiraas <naufalfiraas09@gmail.com>
2022-05-14 10:39:18 +0700

    styling container waktu2 sholat

--- lib/views/sholatpage/sholat_page.dart
+++ lib/views/sholatpage/sholat_page.dart
@@ -1,7 +1,62 @@
 import 'package:flutter/material.dart';
 
-class SholatPage extends StatelessWidget {
+class SholatPage extends StatefulWidget {
   const SholatPage({Key? key}) : super(key: key);
+
+  @override
+  State<SholatPage> createState() => _SholatPageState();
+}
+
+class _SholatPageState extends State<SholatPage> {
+  final List<String> prays = [
+    'Imsak',
+    'Shubuh',
+    'Syuruk',
+    'Dhuhur',
+    'Ashar',
+    'Magrhib',
+    "Isya'",
+  ];
+
+  late final List<Container> praysContainers;
+
+  @override
+  void initState() {
+    super.initState();
+    praysContainers = List.generate(prays.length, (index) {
+      return Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(index == 0 ? 15 : 0),
+            bottom: Radius.circular(index == prays.length - 1 ? 15 : 0),
+          ),
+          color: index % 2 != 0 ? Colors.white : Colors.transparent,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              prays[index],
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                fontFamily: 'Roboto',
+              ),
+            ),
+            const Text(
+              '00 : 00',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                fontFamily: 'Roboto',
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -124,7 +179,7 @@ class SholatPage extends StatelessWidget {
                 ],
               ),
               const SizedBox(
-                height: 28,
+                height: 20,
               ),
               const Text(
                 'Kota Malang',
@@ -144,6 +199,26 @@ class SholatPage extends StatelessWidget {
                   fontSize: 18,
                   fontWeight: FontWeight.w500,
                   fontFamily: 'Roboto',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: const [
+                    BoxShadow(
+                      spreadRadius: 1,
+                      blurRadius: 3,
+                      offset: Offset(0, 1),
+                      color: Color(0x10000000),
+                    ),
+                  ],
+                  color: const Color(0x2000C537),
+                ),
+                child: Column(
+                  children: praysContainers,
                 ),
               ),
             ],

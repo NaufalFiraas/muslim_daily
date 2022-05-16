3d790a7e3da3f82b91cf06f4e0fb598727d6b390
NaufalFiraas <naufalfiraas09@gmail.com>
2022-05-16 15:50:17 +0700

    menambahkan addsholattime states

--- lib/blocs/sholatpage_blocs/add_sholat_time/add_sholat_time_bloc.dart
+++ lib/blocs/sholatpage_blocs/add_sholat_time/add_sholat_time_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:muslim_daily/data/models/sholatpage_model.dart';
 
 part 'add_sholat_time_event.dart';
 part 'add_sholat_time_state.dart';
--- lib/blocs/sholatpage_blocs/add_sholat_time/add_sholat_time_state.dart
+++ lib/blocs/sholatpage_blocs/add_sholat_time/add_sholat_time_state.dart
@@ -4,7 +4,24 @@ abstract class AddSholatTimeState extends Equatable {
   const AddSholatTimeState();
 }
 
-class AddSholatTimeInitial extends AddSholatTimeState {
+//  Loading
+class AddSholatTimeLoading extends AddSholatTimeState {
   @override
   List<Object> get props => [];
+}
+
+//  Success
+class AddSholatTimeSuccess extends AddSholatTimeState {
+  final SholatpageModel sholatpageModel;
+
+  const AddSholatTimeSuccess(this.sholatpageModel);
+
+  @override
+  List<Object?> get props => [sholatpageModel];
+}
+
+//  Failed
+class AddSholatTimeFailed extends AddSholatTimeState {
+  @override
+  List<Object?> get props => [];
 }

3daf8623b97eff608057437258e50eab58ba9660
NaufalFiraas <naufalfiraas09@gmail.com>
2022-05-16 16:03:36 +0700

    menambahkan AddSholatTimeBloc

--- lib/blocs/sholatpage_blocs/add_sholat_time/add_sholat_time_bloc.dart
+++ lib/blocs/sholatpage_blocs/add_sholat_time/add_sholat_time_bloc.dart
@@ -1,16 +1,29 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:muslim_daily/data/models/sholatpage_model.dart';
+import 'package:muslim_daily/data/repositories/sholatpage_repositories.dart';
 
 part 'add_sholat_time_event.dart';
+
 part 'add_sholat_time_state.dart';
 
 class AddSholatTimeBloc extends Bloc<AddSholatTimeEvent, AddSholatTimeState> {
-  AddSholatTimeBloc() : super(AddSholatTimeInitial()) {
-    on<AddSholatTimeEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final SholatpageRepositories sholatpageRepositories;
+
+  AddSholatTimeBloc(this.sholatpageRepositories)
+      : super(AddSholatTimeLoading()) {
+    on<AddSholatTimeEvent>((event, emit) => _addSholatTime(event, emit));
+  }
+
+  void _addSholatTime(
+      AddSholatTimeEvent event, Emitter<AddSholatTimeState> emit) async {
+    emit(AddSholatTimeLoading());
+    SholatpageModel? sholatpageModel =
+        await sholatpageRepositories.getSholatData();
+    sholatpageModel == null
+        ? emit(AddSholatTimeFailed())
+        : emit(
+            AddSholatTimeSuccess(sholatpageModel),
+          );
   }
 }

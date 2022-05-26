a2ad3cf5e044595c7ba173ae44e9d222b7a77b91
Naufal <naufalfiraas09@gmail.com>
2022-05-26 22:00:52 +0700

    menambahkan method getKiblatDirection di sholatpage_repositories dan mengubah property di model ArahKiblat

--- lib/data/models/arah_kiblat.dart
+++ lib/data/models/arah_kiblat.dart
@@ -1,11 +1,11 @@
 import 'package:equatable/equatable.dart';
 
 class ArahKiblat extends Equatable {
-  final double dynamicDirection;
+  final String cityName;
   final double kiblatDirection;
 
-  const ArahKiblat(this.dynamicDirection, this.kiblatDirection);
+  const ArahKiblat(this.cityName, this.kiblatDirection);
 
   @override
-  List<Object?> get props => [dynamicDirection, kiblatDirection];
+  List<Object?> get props => [cityName, kiblatDirection];
 }

888353e742fa07ce4638bdf22ccd58d196c0b62a
Naufal <naufalfiraas09@gmail.com>
2022-05-31 16:38:35 +0700

    menambahkan compass di kiblat direction page dan mengganti versi package flutter_compass

--- lib/views/sholatpage/kiblat_direction_page.dart
+++ lib/views/sholatpage/kiblat_direction_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_compass/flutter_compass.dart';
 import 'sholat_utilities.dart';
 import 'dart:math' as math;
 
@@ -31,19 +32,64 @@ class KiblatDirectionPage extends StatelessWidget {
               width: 300,
               height: 300,
             ),
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                Transform.rotate(
-                  angle: math.pi * 0,
-                  child: Image.asset(
-                    'assets/images/compass.png',
-                    // fit: BoxFit.fill,
-                    width: 200,
-                    height: 200,
-                  ),
-                ),
-              ],
+            StreamBuilder<CompassEvent>(
+              stream: FlutterCompass.events,
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text(
+                      'Gagal mengambil arah',
+                      style: SholatUtilities.textStyling(
+                        size: 16,
+                        color: Colors.black54,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: SizedBox(
+                      height: 30,
+                      width: 30,
+                      child: CircularProgressIndicator(
+                        color: Color(0xFF00C537),
+                      ),
+                    ),
+                  );
+                }
+
+                double? direction = snapshot.data!.heading;
+
+                if (direction == null) {
+                  return Center(
+                    child: Text(
+                      'Perangkat tidak memiliki sensor',
+                      style: SholatUtilities.textStyling(
+                        size: 16,
+                        color: Colors.black54,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                }
+
+                return Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Transform.rotate(
+                      angle: (direction * (math.pi / 180) * -1),
+                      child: Image.asset(
+                        'assets/images/compass.png',
+                        // fit: BoxFit.fill,
+                        width: 200,
+                        height: 200,
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
           ],
         ),

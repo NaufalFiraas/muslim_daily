3e18bc66445d19507f3d0ced22c8682fbb510e61
naufal <naufalfiraas09@gmail.com>
2022-06-03 18:18:10 +0700

    mengubah reminder icon cubit dan state

--- lib/blocs/sholatpage_blocs/reminder_icon/reminder_icon_cubit.dart
+++ lib/blocs/sholatpage_blocs/reminder_icon/reminder_icon_cubit.dart
@@ -1,29 +1,28 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:muslim_daily/data/repositories/sholatpage_repositories.dart';
 
 part 'reminder_icon_state.dart';
 
-class ReminderIconCubit extends HydratedCubit<ReminderIconState> {
-  List<bool> reminderValues;
+class ReminderIconCubit extends Cubit<IconSholatReminderState> {
+  late final SholatpageRepositories repo;
 
-  ReminderIconCubit(this.reminderValues)
-      : super(ReminderIconOff(reminderValues));
-
-  void setReminder(int index, bool value) {
-    state.isReminderOn[index] = value;
-    value
-        ? emit(ReminderIconOff(state.isReminderOn))
-        : emit(ReminderIconOn(state.isReminderOn));
+  ReminderIconCubit([SholatpageRepositories? optionalRepo])
+      : super(const IconSholatReminderChange()) {
+    repo = optionalRepo ?? SholatpageRepositories();
   }
 
-  @override
-  ReminderIconState? fromJson(Map<String, dynamic> json) {
-    return ReminderIconState(json['isReminderOn']);
+  void setIconReminderValue(bool value) {
+    emit(IconSholatReminderChange(value));
   }
 
-  @override
-  Map<String, dynamic>? toJson(ReminderIconState state) {
-    return state.toMap();
+  void saveStatusToPref(int index, bool value) async {
+    await repo.saveIconStatusToProvider(index, value);
+  }
+
+  void getStatusFromPref(int index) async {
+    bool? result = await repo.getIconStatusFromProvider(index);
+    emit(IconSholatReminderChange(result));
   }
 }
--- lib/blocs/sholatpage_blocs/reminder_icon/reminder_icon_state.dart
+++ lib/blocs/sholatpage_blocs/reminder_icon/reminder_icon_state.dart
@@ -1,24 +1,37 @@
 part of 'reminder_icon_cubit.dart';
 
-class ReminderIconState extends Equatable {
-  final List<bool> isReminderOn;
+abstract class IconSholatReminderState extends Equatable {
+  final bool? status;
 
-  const ReminderIconState(this.isReminderOn);
+  const IconSholatReminderState(this.status);
+}
 
-  Map<String, dynamic> toMap() {
-    return {
-      'isReminderOn': isReminderOn,
-    };
-  }
+class IconSholatReminderChange extends IconSholatReminderState {
+  const IconSholatReminderChange([bool? status]) : super(status);
 
   @override
-  List<Object?> get props => [isReminderOn];
+  List<Object?> get props => [status];
 }
 
-class ReminderIconOn extends ReminderIconState {
-  const ReminderIconOn(List<bool> isReminderOn) : super(isReminderOn);
-}
-
-class ReminderIconOff extends ReminderIconState {
-  const ReminderIconOff(List<bool> isReminderOn) : super(isReminderOn);
-}
+// class ReminderIconState extends Equatable {
+//   final List<bool> isReminderOn;
+//
+//   const ReminderIconState(this.isReminderOn);
+//
+//   Map<String, dynamic> toMap() {
+//     return {
+//       'isReminderOn': isReminderOn,
+//     };
+//   }
+//
+//   @override
+//   List<Object?> get props => [isReminderOn];
+// }
+//
+// class ReminderIconOn extends ReminderIconState {
+//   const ReminderIconOn(List<bool> isReminderOn) : super(isReminderOn);
+// }
+//
+// class ReminderIconOff extends ReminderIconState {
+//   const ReminderIconOff(List<bool> isReminderOn) : super(isReminderOn);
+// }
